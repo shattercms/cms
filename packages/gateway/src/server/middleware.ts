81c64c0ed4f79c56c3f169be0ed51c62ad592364
Florian Pallas <mail@pallas-florian.de>
2021-03-22 16:37:56 +0100

    fix: fixed an issue where it was possible to change the scope by using graphql aliases

--- packages/gateway/src/server/middleware.ts
+++ packages/gateway/src/server/middleware.ts
@@ -1,45 +1,19 @@
 import { ShatterContext } from '@shattercms/types';
 import { MiddlewareFn } from 'type-graphql';
 import { AuthenticationError } from 'apollo-server-express';
+import fs from 'fs';
 
 export const authMiddleware: MiddlewareFn<ShatterContext> = async (
   { context, info, root },
   next
 ) => {
-  let node = info.path;
-  let stages: string[] = [];
-
-  // Root Path (Query, Mutation, ...)
-  if (!node.prev && node.typename) {
-    const type = node.typename?.toLowerCase();
-    const action = node.key.toString();
-    stages = [type, action];
-  }
-
-  /*
-  // Regular Paths (user.id, user.posts.title, ...)
-  if (node.prev) {
-    stages.unshift(node.key.toString());
-  }
-  while (node.prev) {
-    if (!node.prev.typename && node.typename) {
-      stages.unshift(node.typename.toLowerCase());
-      break;
-    }
-    node = node.prev;
-    if (node.typename) {
-      stages.unshift(node.key.toString());
-    }
-  }
-  */
-
-  // Only process root scopes
-  if (stages.length < 1) {
+  // Only process root fields
+  if (info.parentType.name.toLowerCase() !== info.operation.operation)
     return next();
-  }
 
   // Construct scope and check permission
-  const scope = stages.join('.');
+  const scope = `${info.operation.operation}.${info.fieldName}`;
+
   const permission = context.config.permissions[scope];
   const hasAccess = await context.auth.hasPermission(
     { scope, permission, data: root },
